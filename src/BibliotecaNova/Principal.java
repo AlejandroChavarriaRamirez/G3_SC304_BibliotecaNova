ce22a8d46935e48fc9391fd828a99767b106fe91
Alejandro Chavarria <alechavarria2022@gmail.com>
2026-07-22 20:49:54 -0600

    Proyecto Biblioteca Nova

---
+++ src/BibliotecaNova/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package BibliotecaNova;
+
+/**Integrantes:
+ * Alejandro Chavarria Ramirez
+ * Andres Brenes Herrera
+ * Saul Amir Alvarado Montero
+ * Josel Pablo Vargas Caldero
+ *
+ * @author aleja
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
